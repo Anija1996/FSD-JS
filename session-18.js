b4256df20b9e8716fcaaa7ea170fe99974082059
Anija1996 <anijasarath1996@gmail.com>
2026-08-03 13:23:19 +0530

    Functions and Function types

---
+++ session-18.js
@@ -0,0 +1,36 @@
+const order = {
+    customerName : "Sarath",
+    distance : 10,
+    item : [
+        { name : "Fried Rice", price : 250, quantity : 3},
+        { name : "Chilli Chicken", price : 350}
+    ]
+}
+
+const deliveryCharge = (distance) => {
+    return distance > 7 ? 70 : 50;
+}
+// console.log(deliveryCharge);
+
+
+const applyDiscount = (price,discount=0) => price - price * discount;
+
+const festivalDiscount = (total) => applyDiscount(total, 0.2);
+
+function placeOrder(order, discountType,callbackFn) {
+    //Destructuring
+    const {
+        customerName,
+        distance,
+        item
+    } = order;
+
+    const totalAmount = item.reduce((sum,{price,quantity=1}) => sum + price * quantity,0);
+    const discountedAmount = discountType(totalAmount);
+    const finalAmount = discountedAmount + deliveryCharge(distance);
+    callbackFn(customerName,finalAmount);
+}
+
+placeOrder(order, festivalDiscount, (name, finalAmount) => {
+    console.log(`Order Confirmed for ${name}. \nPay Rs.${finalAmount}`);
+})
